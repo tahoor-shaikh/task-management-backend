38022d257b6d385b035b8db8602bbebdb504d9b2
Tahoor Shaikh <shaikhdanish4452@gmail.com>
2025-02-18 22:22:02 +0530

    Add Vercel configuration

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ app.use("/tasks", require("./routes/tasks"));
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
